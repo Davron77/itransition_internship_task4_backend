c4549de6130ab1866bfe5d9f2fb65d1b70fb59c3
davron77 <davronbekq467@gmail.com>
2024-09-26 00:51:42 +0500

    set up

---
+++ routes/authRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const {
+  registerUser,
+  loginUser,
+  getUsers,
+} = require("../controllers/authController");
+const authMiddleware = require("../middlewares/authMiddleware");
+
+const router = express.Router();
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/users", authMiddleware, getUsers);
+
+module.exports = router;
